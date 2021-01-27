24aaadcf4119aa12981ce2abb0499513fda46e04
Esteban Moreno <estebanmc2912@gmail.com>
2021-01-27 16:51:33 -0500

    Initial commit

---
+++ app/src/main/java/com/paloit/validator/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.paloit.validator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var numbersByElement : Map<Int,Int> ? = null
+    private var listNumbers : ArrayList<Int> ? = null
+    var validation : String ? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+        listNumbers = ArrayList()
+
+    }
+
+    private fun setListeners() {
+        btn_lucknumber.setOnClickListener {
+            onClickBtnLuckyNumber(it)
+        }
+    }
+
+
+    fun onClickBtnLuckyNumber(v : View){
+        if(checkValidation(et_cardnuber.text.toString())){ validation = "true" }else{ validation = "false" }
+        Toast.makeText(this@MainActivity, validation, Toast.LENGTH_LONG).show()
+        mostRepitNumber(et_cardnuber.text.toString())
+    }
+
+    fun checkValidation(str : String) : Boolean{
+        if ( (str.length>=14) and (str.length<=16) ){ return true }
+        return false
+    }
+
+    fun mostRepitNumber(str : String) : String{
+
+        for(i in str){ listNumbers?.add(i.toString().toInt()) }
+
+        numbersByElement = listNumbers?.groupingBy { it }?.eachCount()
+        println("Numbers and count of many repeats")
+        /// Log Print to each numbers
+        println(numbersByElement)
+
+        // This is an optional function but i don't want to use it beacuse increments algorithm complex O(n2)
+       /* for (i in mutableList!!){
+            var cont = 1
+            for (j in listNumbers!!){
+                if (i==j){
+                    cont++
+                }
+                Log.d("number " +i, cont.toString())
+            }
+        }*/
+
+        var mostrepetitivenumber = listNumbers?.groupingBy { it }?.eachCount()?.toList()?.sortedByDescending { it.second }?.take(1)
+
+        return mostrepetitivenumber.toString()
+    }
+
+}
---
+++ app/src/test/java/com/paloit/validator/MainActivityTest.kt
@@ -0,0 +1,22 @@
+package com.paloit.validator
+
+import junit.framework.TestCase
+
+class MainActivityTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    fun testOnCreate() {}
+
+    fun testOnClickBtnLuckyNumber() {}
+
+    fun testCheckValidation() {
+        //assertEquals(,testCheckValidation())
+    }
+
+    fun testMostRepitNumber() {
+        //assertEquals(,testMostRepitNumber())
+    }
+}
